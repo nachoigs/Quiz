9dc68488ba439cbcf0a95a5d0f8a36fd9bea7f2c
nachoigs <ignacio.gomez.saez@alumnos.upm.es>
2015-05-01 04:47:51 +0200

    Despliegue en Heroku

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/quizes/question', quizController.question);
-router.get('/quizes/question', quizController.answer);
+router.get('/quizes/answer', quizController.answer);
 router.get('/authors', function(req, res, next){
 	res.render('authors', {title: 'autores'});
 });
